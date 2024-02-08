b7fa3dd99b62d0016895beac55c983c909e899a7
Kiol-Ice <ramadiel2001@gmail.com>
2024-02-08 09:55:01 +0100

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import booksJson from '../../assets/books.json';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButtonModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  bookNumber = booksJson.length;
 }
