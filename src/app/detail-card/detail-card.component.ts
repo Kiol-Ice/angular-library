886f2866855edce47c805e813f2634affb4ccb70
Kiol-Ice <ramadiel2001@gmail.com>
2024-02-11 15:01:13 +0100

    back nav on detail

--- src/app/detail-card/detail-card.component.ts
+++ src/app/detail-card/detail-card.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-detail-card',
@@ -18,4 +19,10 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 export class DetailCardComponent {
   @Input() headerText: string;
   @Input() backRoute: string;
+
+  constructor(private location: Location) { }
+
+  public back() {
+    this.location.back();
+  }
 }
